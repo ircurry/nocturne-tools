4e0a033a7858a936d19518da706b936b407114ae
Ian Curran <icurran@protonmail.com>
2024-09-28 18:29:09 -0400

    main: basic implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io::{self, Write};
+
 mod monitors;
 
-fn main() {}
+fn main() {
+    let profile = monitors::Profile::new_profile1();
+    println!("Configuring Monitors for Profile {}", profile.name);
+    let profile_output = profile.configure_monitors();
+    for output in profile_output {
+	io::stdout().write_all(&output.stdout).expect("failed to write to stdout");
+	io::stderr().write_all(&output.stderr).expect("failed to write to stderr");
+    }
+}
